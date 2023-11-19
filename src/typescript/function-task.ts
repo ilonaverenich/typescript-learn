4df77865ad243a152cfcc4ae99b6c069e080712c
Ilona <Ilonaverenich@tut.by>
2023-11-19 19:53:08 +0300

    function-task

---
+++ src/typescript/function-task.ts
@@ -0,0 +1,20 @@
+//?: опциональный
+//Типизация функций
+function slice (str:string, start:number, end?:number):string{
+    let newStr = '';
+
+    let lastIndex: number;
+
+    if(end){
+        lastIndex = end > str.length? str.length:end;
+    }
+    else {
+        lastIndex = str.length;
+    }
+
+    for (let i = start; i<lastIndex;i++){
+        newStr += str[i];
+    }
+
+    return newStr;
+}
