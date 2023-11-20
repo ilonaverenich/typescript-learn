48d0dd9168486cbe20feb5c0ba5527135e35f068
Ilona <Ilonaverenich@tut.by>
2023-11-20 13:55:39 +0300

    added an object-task

---
+++ src/typescript/object-task.ts
@@ -0,0 +1,36 @@
+interface User{
+    login :string,
+    email:string,
+    password:string,
+    isOnline:boolean,
+    lastVisited?:Date,
+}
+interface Admin{
+    login: string,
+    email:string,
+    password:string,
+    isOnline: boolean,
+    lastVisited?:Date
+
+}
+const user:User ={
+    login: 'ilona',
+    email:'verenich@mail.ru',
+    password:'1234!!!',
+    isOnline:true,    
+}
+
+const admin :Admin ={
+    login: 'Женя',
+    email:'hoholkoh@mail.ru',
+    password:'1231114!!!@#$!',
+    isOnline:true,    
+}
+
+function login(user:{login:string, password:string}){
+    if(user.login.length>0 && user.password.length>0){
+    console.log('hello',user.login)
+    }
+}
+login(user)
+login(admin)
