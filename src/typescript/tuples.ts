11269acae8c551a37dbe159d9e008a618a8d0ef9
Ilona <Ilonaverenich@tut.by>
2023-11-23 15:44:45 +0300

    learnt tuples

---
+++ src/typescript/tuples.ts
@@ -0,0 +1,10 @@
+//Tuples - Кортежи (содежит разрозненный тип)
+
+
+const pairs:[string,string][] = [['key','value'],['key2','value']];
+const data:[number,boolean,string] =[1,true,'5f'];
+
+
+//csv - для использования
+const doc :[string,string,number,Date][]=[];
+doc.push(['Ilona','Verenic',5,new Date(1995,5,5)])
